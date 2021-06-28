e481911eccf7f7b38d1342a3757bd56bc0cf70b6
Anya <anut.egorova2012@yandex.ru>
2021-06-28 22:37:03 +0300

    Uploaded files

---
+++ public/js/main.js
@@ -0,0 +1,37 @@
+function slowScroll(id) {//в параметр передаем id элемента, к которому хотим проскроллить
+    $("html, body").animate({//анимация для html, body - тегов
+        scrollTop: $(id).offset().top - 50 //всю нашу страничку скроллим к этому объекту. Для этого берем отступ
+        // этого объекта от начала странички.
+    }, 500);
+    return false;//чтобы не обновлялась страничка
+}
+
+$(".header-top .menu").on("click", function () {
+    if ($("header .mobile-menu").is(":visible"))//виден ли сейчас mobile-menu
+        $(this).html('<i class="fas fa-bars"></i>');
+    else
+        $(this).html('<i class="fas fa-times"></i>');
+
+    $("header .mobile-menu").slideToggle();//Плавно раскрывается и закрывается
+});
+
+$(document).ready(function () {
+    $('.slider').slick({
+        prevArrow: $('.prev'),
+        nextArrow: $('.next')
+    });
+});
+
+$("#subscribe").on("click", function () {
+    let email = $("#email").val();//получаем email пользователя
+    email = email.trim()// без лишних пробелов
+    if (email.split("@").length != 2 || email.split(".").length < 2) {//email разбили по символу @ (массив должен
+        // состоять из 2 элементов)
+        $("#sub_form label").text("Вы ввели неверный email");
+        $("#sub_form label").fadeIn();
+    }
+
+    setTimeout(function () {
+        $("#sub_form label").fadeOut();
+    }, 1500);
+});
